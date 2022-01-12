9a1d4d87d5b2bfab19833e91bef089c81bc76f0a
Atharva <atharvashendef35@gmail.com>
2022-01-12 19:27:36 +0530

    change to font color

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -33,7 +33,7 @@ const ScrollableChat = ({ messages }) => {
             <span
               style={{
                 backgroundColor: `${
-                  m.sender._id === user._id ? "white" : "#B9F5D0"
+                  m.sender._id === user._id ? "#B9F5D0" : "white"
                 }`,
                 marginLeft: isSameSenderMargin(messages, m, i, user._id),
                 marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
